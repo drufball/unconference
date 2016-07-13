bf2b74fb637f98cf8ccd692150a789946325cf40
drufball <knox.dru@gmail.com>
2016-07-13 14:13:45 -0700

    Changed port to process.env.port

--- server.js
+++ server.js
@@ -10,6 +10,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(80, function () {
-    console.log('Example app listening on port 3000!');
-});
+app.listen(process.env.PORT);
