967e17b96ecb5ac6894f1a4993818ca5322b4d5e
drufball <knox.dru@gmail.com>
2016-07-13 14:45:34 -0700

    added a log statement outside everything to hopefully tell me if this file even runs. UGH

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ var path = require('path');
 
 var app = express();
 
+console.log("SERVER IS FUCKING RUNNING");
+
 app.use('/src', express.static('src'));
 app.use('/polymer', express.static('polymer'));
 
-app.get('*', (req, res) => {
+app.get('/health', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, function() {
+    console.log("listening on " + process.env.PORT);
+});
