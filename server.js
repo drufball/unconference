912c0ded9e0acce960db498603f28daa54e9c8db
drufball <knox.dru@gmail.com>
2016-07-13 14:06:57 -0700

    Changed port to 80

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, function () {
+app.listen(80, function () {
     console.log('Example app listening on port 3000!');
 });
