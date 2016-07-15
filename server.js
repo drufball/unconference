aa108b0dc25e33848ad4518d6af830ec7c4b3a80
drufball <knox.dru@gmail.com>
2016-07-15 12:59:45 -0700

    Set up a new route to providide HTML from a README.md file. Loads in the readme file line by line, parses/renders it. Then wraps it into a single html element and pushes that down. Had to add a small fix to handle '<' and '>'. Then added a bit of padding to make the element look better.

--- server.js
+++ server.js
@@ -34,6 +34,46 @@ var app = express();
 app.use('/src', express.static('src'));
 app.use('/polymer', express.static('polymer'));
 
+app.get('/readme-content', (req, res) => {
+    var readline = require('readline');
+    var fs = require('fs');
+    var renderedMD = "";
+    var marked = require('marked');
+
+    var rl = readline.createInterface({
+        input: fs.createReadStream('README.md')
+    });
+
+    rl.on('line', (line) => {
+        while(line.includes('<')) {
+            line = line.replace('<', '&#60;');
+        }
+        while(line.includes('>')) {
+            line = line.replace('>', '&#62;');
+        }
+        renderedMD += marked(line);
+    });
+
+    rl.on('close', () => {
+        console.log(renderedMD);
+        res.send("<div>" + renderedMD + "</div>");
+    });
+
+
+    // var fs = require('fs');
+    // fs.readFile('README.md', (err, data) => {
+    //
+    //     marked.setOptions({
+    //         gfm: false
+    //     });
+    //     console.log("returning " + __dirname + '/readme-content.html');
+    //     marked(data.toString(), (err, content) => {
+    //         if(err) throw err;
+    //         res.send(content);
+    //     });
+    // });
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
