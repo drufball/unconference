4bf5c3218688df8c8e96f53d63c4c420956127e9
drufball <knox.dru@gmail.com>
2016-07-25 19:17:52 -0700

    cleaning up everything from schedule and some database additions

--- server.js
+++ server.js
@@ -28,11 +28,73 @@ var lex = LEX.create({
 
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
+
+// Database setup
+var PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-load'));
+app.use('/db', require('express-pouchdb')(PouchDB));
+
+// Databases
+var talkdb = new PouchDB('talks');
 
 app.use('/src', express.static('src'));
 app.use('/polymer', express.static('polymer'));
+
+// Database helpers
+function add_talk(data) {
+    return new Promise((fulfill, refect) => {
+        console.log("before parse - " + data.title);
+        talkdb.post(data).then(response => {
+            console.log("made it");
+            fulfill(response);
+        }).catch(error => {
+            reject(error);
+        });
+    });
+}
+function get_talks(limit) {
+    return new Promise((fulfill, reject) => {
+        if(limit === "all") {
+            var options = {limit:10, include_docs: true};
+            talkdb.allDocs(options, function (err, response) {
+                if (err == null) {
+                    fulfill(response);
+                } else {
+                    console.log(error);
+                    reject(err);
+                }
+            });
+        }
+        else {
+            reject("Single access not yet supported");
+        }
+    });
+}
+
+// TODO: make sure npm install works for pouchdb
+// TODO: switch add talks to post from get
+
+// Database endpoints
+app.post('/talks/add', (req, res) => {
+    add_talk(req.body)
+    .then(result => {
+        res.status(201).send(result);
+    }).catch(error => {
+        res.status(500).send(error);
+    });
+});
+app.get('/talks/:talk', (req, res) => {
+    get_talks(req.params.talk)
+    .then(result => {
+        res.json(result);
+    }).catch(error => {
+        res.send(error);
+    });
+});
 
 app.get('/readme-content', (req, res) => {
     var readline = require('readline');
@@ -58,20 +120,10 @@ app.get('/readme-content', (req, res) => {
         console.log(renderedMD);
         res.send("<div>" + renderedMD + "</div>");
     });
+});
 
-
-    // var fs = require('fs');
-    // fs.readFile('README.md', (err, data) => {
-    //
-    //     marked.setOptions({
-    //         gfm: false
-    //     });
-    //     console.log("returning " + __dirname + '/readme-content.html');
-    //     marked(data.toString(), (err, content) => {
-    //         if(err) throw err;
-    //         res.send(content);
-    //     });
-    // });
+app.get('/schedule-content', (req, res) => {
+    res.sendFile(path.join(__dirname + '/schedule.json'));
 });
 
 app.get('*', (req, res) => {
